eeb8b396b2dd2073211811e305d7ba443085fffb
Nanthipat052 <142304991+Nanthipat052@users.noreply.github.com>
2023-10-03 22:31:38 +0700

    Add files via upload

---
+++ int101_hw1_66130500052/int101s2023h01/src/main/java/work03/MyUtil.java
@@ -0,0 +1,12 @@
+package work03;
+
+public class MyUtil {
+    public static double calculateBMI(double weight, double height) {
+        return weight / (height * height);
+    }
+
+    public static int average(int V1, int V2, int V3) {
+        return (V1 + V2 + V3) / 3;
+    }
+
+}
